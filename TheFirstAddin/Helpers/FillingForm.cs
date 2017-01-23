87f9e59137d18c5364cd5af6e7b4910626c65944
Nozd <prosvet_egor@mail.ru>
2017-01-24 00:21:30 +0300

    Cell format

--- TheFirstAddin/Helpers/FillingForm.cs
+++ TheFirstAddin/Helpers/FillingForm.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Excel = Microsoft.Office.Interop.Excel;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Excel;
+using Microsoft.Office.Tools.Ribbon;
 
-namespace TheFirstAddin.Helpers
+namespace TheFirstAddin
 {
-    class FillingForm
+    public static class FillingForm
     {
+        public static void FillForm()
+        {
+            //Excel.Range currentCell = this.Application.ActiveCell;
+
+            //Excel.Worksheet sht = (Excel.Worksheet)ActiveSheet;
+            //sht.Cells[1, 1] = "HELLO";
+        }
     }
 }
--- TheFirstAddin/ThisAddIn.cs
+++ TheFirstAddin/ThisAddIn.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -41,29 +42,88 @@ namespace TheFirstAddin
             return Globals.Factory.GetRibbonFactory().CreateRibbonManager(new IRibbonExtension[] { ribbon });
         }
 
-        //Passport Button click
+#region Passport Button click
         private void ribbon_ButtonClicked()
         {
             Excel.Range range = Globals.ThisAddIn.Application.Selection as Excel.Range;
-            if (!Validate.ValidateRows(range))
+            if (!Validation.ValidateRows(range))
             {
-                return;
+                //return;
             }
 
-            //int rowLength = 5;
-            //for(int i = 0; i < rowLength; i++)
-            ////foreach (object cell in range.Cells)
-            //{
-            //    try
-            //    {
-            //        MessageBox.Show(((Excel.Range)range.Cells[i]).Value2.ToString());
-            //    }
-            //    catch
-            //    {
-            //        MessageBox.Show("NULL VALUE");
-            //    }
-            //}
-            
+            //Create new workBook
+            this.Application.Visible = true;
+            Excel.Workbook newWorkbook = this.Application.Workbooks.Add(missing);
+            newWorkbook.SaveAs(@"D:\Book1.xlsx", missing,
+                missing, missing, missing, missing, Excel.XlSaveAsAccessMode.xlNoChange,
+                missing, missing, missing, missing, missing);
+
+            //Fill form
+            const double relHeight = 5.1;
+            Excel.Worksheet activeSheet = Globals.ThisAddIn.Application.ActiveSheet;
+            //
+            var activeCell = activeSheet.get_Range("A4", "G7");
+            activeCell.WrapText = true;
+            activeCell.VerticalAlignment = 1;
+            activeSheet.get_Range("A4", "A6").HorizontalAlignment = 1;
+            activeSheet.get_Range("A1", "A1").ColumnWidth = 8.4;
+            activeSheet.get_Range("B1", "B1").ColumnWidth = 8.4;
+            activeSheet.get_Range("C1", "C1").ColumnWidth = 8.4;
+            activeSheet.get_Range("D1", "D1").ColumnWidth = 45;
+            activeSheet.get_Range("E1", "E1").ColumnWidth = 8.4;
+            activeSheet.get_Range("F1", "F1").ColumnWidth = 8.4;
+            activeSheet.get_Range("G1", "G1").ColumnWidth = 8.4;
+            //
+            activeSheet.Cells[1,1] = "Упаковочный лист*";
+            activeCell = activeSheet.get_Range("A1", "G1");
+            activeCell.Merge(true);
+            activeCell.EntireRow.RowHeight = 31.5;
+            activeCell.Font.Size = 24;
+            activeCell.Font.Bold = true;
+            activeCell.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            activeCell.VerticalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            //
+            activeSheet.Cells[2, 1] = "ДПМ 01/30к   №____________";
+            activeCell = activeSheet.get_Range("A2", "G2");
+            activeCell.Merge(true);
+            activeCell.Font.Size = 14;
+            activeCell.Font.Bold = true;
+            activeCell.HorizontalAlignment = 3;
+            activeCell.VerticalAlignment = 3;
+            //
+            activeSheet.Cells[4, 1] = "1";
+            activeSheet.Cells[4, 2] = ".";
+            activeSheet.Cells[4, 3] =
+                "Дверь в сборе с установленным замком, ригелем, петлевыми подшипниками и ответной планкой";
+            activeSheet.get_Range("C4","D4").Merge(true);
+            activeSheet.get_Range("A4", "A4").RowHeight = Math.Truncate(activeSheet.get_Range("A6", "A6").RowHeight / relHeight);
+            activeSheet.Cells[4, 5] = "1";
+            activeSheet.Cells[4, 7] = "к-кт";
+            //
+            activeSheet.Cells[5, 1] = "2";
+            activeSheet.Cells[5, 2] = ".";
+            activeSheet.Cells[5, 3] = "Цилиндр замка с комплектом ключей и винтом";
+            activeSheet.get_Range("C5", "D5").Merge(true);
+            activeSheet.get_Range("A5", "A5").RowHeight = Math.Truncate(activeSheet.get_Range("A5", "A5").RowHeight / relHeight);
+            activeSheet.Cells[5, 5] = "1";
+            activeSheet.Cells[5, 7] = "к-кт";
+            //
+            activeSheet.Cells[6, 1] = "3";
+            activeSheet.Cells[6, 2] = ".";
+            activeSheet.Cells[6, 3] = "Ручка со стяжными винтами и накладками";
+            activeSheet.get_Range("C6", "D6").Merge(true);
+            activeSheet.get_Range("A6", "A6").RowHeight = Math.Truncate(activeSheet.get_Range("A6", "A6").RowHeight/relHeight);
+            activeSheet.Cells[6, 5] = "1";
+            activeSheet.Cells[6, 7] = "к-кт";
+            //
+           
+
+
+            newWorkbook.Save();
+
+           
+
         }
+#endregion
     }
 }
